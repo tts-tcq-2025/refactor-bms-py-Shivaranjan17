8aff0b784215fc6be18c7627e61e20bc0418d678
Shivaranjan17 <shiv.manali1@gmail.com>
2025-07-29 10:55:23 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,37 +1,35 @@
-
 from time import sleep
 import sys
 
+def is_vital_in_range(value, lower, upper):
+    return lower <= value <= upper
+
+def print_alert(message):
+    print(message)
+    blink_alert()
+
+def blink_alert():
+    for _ in range(6):
+        print('\r* ', end='')
+        sys.stdout.flush()
+        sleep(1)
+        print('\r *', end='')
+        sys.stdout.flush()
+        sleep(1)
 
 def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+    all_ok = True
+
+    if not is_vital_in_range(temperature, 95, 102):
+        print_alert('Temperature critical!')
+        all_ok = False
+
+    if not is_vital_in_range(pulseRate, 60, 100):
+        print_alert('Pulse Rate is out of range!')
+        all_ok = False
+
+    if not is_vital_in_range(spo2, 90, 100):
+        print_alert('Oxygen Saturation out of range!')
+        all_ok = False
+
+    return all_ok
